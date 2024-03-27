edef2ec746ac3aa7d7ea0bec8a4ba37dea8ab6dd
Jinseo Jeong <js020315@gmail.com>
2024-03-27 11:28:11 +0900

    Spring Initializr
    Default code

---
+++ src/main/java/com/elice/team4/singleShop/SingleShopApplication.java
@@ -0,0 +1,13 @@
+package com.elice.team4.singleShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingleShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SingleShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elice/team4/singleShop/SingleShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elice.team4.singleShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SingleShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
