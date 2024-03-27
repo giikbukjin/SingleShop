3ade84be12d3bd41463767091868119e4595410f
wjcho <wj930303@gmail.com>
2024-03-28 00:21:08 +0900

    Feat: category entity initial commit.

--- src/main/java/com/elice/team4/singleShop/category/entity/Category.java
+++ src/main/java/com/elice/team4/singleShop/category/entity/Category.java
@@ -1,7 +1,32 @@
 package com.elice.team4.singleShop.category.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 @Entity
+@Getter
+@AllArgsConstructor
 public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String categoryName;
+
+    private String categoryContent;
+
+    private String categoryTheme;
+
+    // TODO: Product 다대일 관계 필드 선언
+
+    public Category(String categoryName, String categoryContent, String categoryTheme) {
+        this.categoryName = categoryName;
+        this.categoryContent = categoryContent;
+        this.categoryTheme = categoryTheme;
+    }
 }
