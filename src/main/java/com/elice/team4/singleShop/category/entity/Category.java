cda3a8b844572b79e81bba5fd3e64db311686a2d
wjcho <wj930303@gmail.com>
2024-03-28 00:25:33 +0900

    Feat: addtion of Setter and Constructor lombok annotations.

--- src/main/java/com/elice/team4/singleShop/category/entity/Category.java
+++ src/main/java/com/elice/team4/singleShop/category/entity/Category.java
@@ -6,10 +6,14 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Getter
+@Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class Category {
 
     @Id
