f8391c836856f6892352ded285848f9388a7da62
wjcho <wj930303@gmail.com>
2024-03-28 09:38:33 +0900

    Feat: CategoryDto.java

---
+++ src/main/java/com/elice/team4/singleShop/category/dto/CategoryDto.java
@@ -0,0 +1,14 @@
+package com.elice.team4.singleShop.category.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class CategoryDto {
+    private String categoryName;
+    private String categoryContent;
+    private String categoryTheme;
+}
