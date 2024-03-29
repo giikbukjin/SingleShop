9ce3a7f4f69a6cc264677b1341832648cf3d6c75
Wambatcode <feyfey927@gmail.com>
2024-03-29 16:31:36 +0900

    Feat: User Controller, Service, Repository생성

---
+++ src/main/java/com/elice/team4/singleShop/user/controller/UserController.java
@@ -0,0 +1,9 @@
+package com.elice.team4.singleShop.user.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+}
---
+++ src/main/java/com/elice/team4/singleShop/user/service/UserService.java
@@ -0,0 +1,7 @@
+package com.elice.team4.singleShop.user.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+}
