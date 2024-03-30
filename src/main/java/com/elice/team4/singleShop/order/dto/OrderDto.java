550ce778e5c55bae7379bd6f9136421d459ddafa
blaumonde <larazili174@gmail.com>
2024-03-30 14:08:39 +0900

    fitst

---
+++ src/main/java/com/elice/team4/singleShop/order/dto/OrderDto.java
@@ -0,0 +1,13 @@
+package com.elice.team4.singleShop.order.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class OrderDto {
+
+    private int productId;
+
+    private int count;
+}
