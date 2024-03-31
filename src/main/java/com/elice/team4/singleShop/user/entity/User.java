19791b1c8b3b7ceaaee5e9d4662b70af245acc30
Wambatcode <feyfey927@gmail.com>
2024-03-31 16:29:30 +0900

    Feat: User엔티티에 UserDetails인터페이스 구현

--- src/main/java/com/elice/team4/singleShop/user/entity/User.java
+++ src/main/java/com/elice/team4/singleShop/user/entity/User.java
@@ -1,5 +1,9 @@
 package com.elice.team4.singleShop.user.entity;
 
+import com.elice.team4.singleShop.cart.entity.Cart;
+import com.elice.team4.singleShop.order.entity.Order;
+import com.elice.team4.singleShop.product.domain.Product;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -11,8 +15,14 @@ import lombok.Setter;
 import org.hibernate.annotations.ColumnDefault;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Getter
@@ -20,7 +30,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 @EntityListeners(AuditingEntityListener.class)
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -48,22 +58,58 @@ public class User {
     @ColumnDefault("CONSUMER") //TODO: 코치님에게 질문하기(Bean Validation 의존성 추가)
     private Role role;
 
-//    TODO: 매핑필요
-//    @OneToMany(mappedBy = "user")
+
+//    @OneToMany(mappedBy = "user") TODO: 진서님 Product엔티티에 User컬럼 추가 요망
 //    private Product product;
 
-//    TODO: 매핑필요
-//    @OneToOne(mappedBy = "user")
-//    private Cart cart;
+    @OneToOne(mappedBy = "user")
+    private Cart cart;
 
-//    TODO: 매핑필요
-//    @OneToMany(mappedBy = "user")
+//    @OneToMany(mappedBy = "user") TODO: 민상님 Order엔티티 주석 제거 요망
 //    private Order order;
 
     public enum Role{
         CONSUMER,
         SELLER,
         ADMIN
+    }
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority("ROLE_" + this.role.toString()));
+        return authorities;
+    }
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @Override
+    public String getUsername() {
+        return this.name;
+    }
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @Override
+    public boolean isEnabled() {
+        return false;
     }
 
     public void update(String name, String password, String email){
