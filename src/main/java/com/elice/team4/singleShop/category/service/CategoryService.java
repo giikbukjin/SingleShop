2b8ee3cd4eb0a620e36eac0d7a194fdec04fff84
wjcho <wj930303@gmail.com>
2024-03-31 19:18:13 +0900

    Refactor: CategoryService Transactional read-only

--- src/main/java/com/elice/team4/singleShop/category/service/CategoryService.java
+++ src/main/java/com/elice/team4/singleShop/category/service/CategoryService.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-@Transactional
+@Transactional(readOnly = true)
 public class CategoryService {
     private final CategoryRepository categoryRepository;
 
