3d9f6f5d79acf4d2d7c3dc6caa258cdcb4db2d65
Wambatcode <feyfey927@gmail.com>
2024-03-31 20:56:18 +0900

    Feat: PasswordEncode테스트 추가

--- src/main/java/com/elice/team4/singleShop/user/config/SecurityConfig.java
+++ src/main/java/com/elice/team4/singleShop/user/config/SecurityConfig.java
@@ -4,14 +4,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
-public class SecurityConfig extends WebSecurityConfiguration {
+public class SecurityConfig{
 
     @Bean
     public PasswordEncoder passwordEncoder(){
