e623052c2b1faea39c1ad920f02d37e7c9594458
Wambatcode <feyfey927@gmail.com>
2024-03-31 16:13:38 +0900

    Feat: UserMapper 코드드 추가

--- src/main/java/com/elice/team4/singleShop/user/mapper/UserMapper.java
+++ src/main/java/com/elice/team4/singleShop/user/mapper/UserMapper.java
@@ -1,4 +1,10 @@
 package com.elice.team4.singleShop.user.mapper;
 
+import com.elice.team4.singleShop.user.dto.UserDto;
+import com.elice.team4.singleShop.user.entity.User;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
 public interface UserMapper {
+    User UserDtoToUser(UserDto userDto);
 }
