e5c052165ebdf20866a036d2bf825357ae171f86
Wambatcode <feyfey927@gmail.com>
2024-04-03 03:45:48 +0900

    Fix: UserController 어ër 어노테이션 추가 및 Service  의존성 수정

--- src/main/java/com/elice/team4/singleShop/user/controller/UserController.java
+++ src/main/java/com/elice/team4/singleShop/user/controller/UserController.java
@@ -1,13 +1,17 @@
 package com.elice.team4.singleShop.user.controller;
 
-import com.elice.team4.singleShop.user.service.UserServiceImpl;
+import com.elice.team4.singleShop.user.service.SignService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
+@Slf4j
 public class UserController {
-    private final UserServiceImpl userService;
+    private final SignService signService;
 
-    public UserController(UserServiceImpl userService) {
-        this.userService = userService;
+    public UserController(SignService signService) {
+        this.signService = signService;
     }
 }
