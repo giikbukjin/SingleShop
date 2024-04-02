5a6a53f76ef28fb1cfbb8f5370c4b699e4759dff
Wambatcode <feyfey927@gmail.com>
2024-04-03 00:46:36 +0900

    Fix: UserService Class Rename

--- src/main/java/com/elice/team4/singleShop/user/controller/UserController.java
+++ src/main/java/com/elice/team4/singleShop/user/controller/UserController.java
@@ -1,13 +1,13 @@
 package com.elice.team4.singleShop.user.controller;
 
-import com.elice.team4.singleShop.user.service.UserService;
+import com.elice.team4.singleShop.user.service.UserServiceImpl;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
-    private final UserService userService;
+    private final UserServiceImpl userService;
 
-    public UserController(UserService userService) {
+    public UserController(UserServiceImpl userService) {
         this.userService = userService;
     }
 }
