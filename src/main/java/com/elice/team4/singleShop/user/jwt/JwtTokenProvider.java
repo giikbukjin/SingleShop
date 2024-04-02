0d61a2faeefa8f18849138506f78d933f0014d8f
Wambatcode <feyfey927@gmail.com>
2024-04-03 02:20:55 +0900

    Feat: JwtTokenProvider Logger생성

--- src/main/java/com/elice/team4/singleShop/user/jwt/JwtTokenProvider.java
+++ src/main/java/com/elice/team4/singleShop/user/jwt/JwtTokenProvider.java
@@ -8,6 +8,8 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import jakarta.annotation.PostConstruct;
 import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -23,6 +25,7 @@ import java.util.Date;
 
 @Component
 public class JwtTokenProvider {
+    private final Logger LOGGER = LoggerFactory.getLogger(JwtTokenProvider.class);
     private final long TOKEN_VALID_MILISECOND = 1000L * 60 * 60 * 10;
 
     @Value("${key.salt}")
@@ -39,13 +42,16 @@ public class JwtTokenProvider {
 
     @PostConstruct
     protected void init() {
+        LOGGER.info("[init] JwtTokenProvider 내 secretKey 초기화 시작");
         secretKey = Keys.hmacShaKeyFor(salt.getBytes(StandardCharsets.UTF_8));
+        LOGGER.info("[init] JwtTokenProvider 내 SecretKey 초기화 완료");
     }
 
     public String createToken(String userPk, User.Role roles) {
         Claims claims = Jwts.claims().setSubject(userPk);
         claims.put("roles", roles);
         Date now = new Date();
+        LOGGER.info("[createToken] 토큰 생성 완료");
         return Jwts.builder()
                 .setClaims(claims)
                 .setIssuedAt(now)
@@ -55,25 +61,31 @@ public class JwtTokenProvider {
     }
 
     public Authentication getAuthentication(String token) {
+        LOGGER.info("[getAuthentication] 토큰 인증 정보 조회 시작");
         UserDetails userDetails = userDetailsService.loadUserByUsername(this.getUserPk(token));
+        LOGGER.info("[getAuthentication] 토큰 인증 정보 조회 완료, UserDetails UserName : {}", userDetails.getUsername());
         return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
     }
 
     public String getUserPk(String token) {
+        LOGGER.info("[getUsername] 토큰 기반 회원 구별 정보 추출");
         return Jwts.parserBuilder().setSigningKey(secretKey).build()
                 .parseClaimsJws(token).getBody().getSubject();
     }
 
     public String resolveToken(HttpServletRequest req) {
+        LOGGER.info("[resolveToken] HTTP 헤더에서 Token 값 추출");
         return req.getHeader("X-AUTH-TOKEN");
     }
 
     public boolean validateToken(String jwtToken) {
+        LOGGER.info("[validateToken] 토큰 유효 체크 시작 ");
         try {
             Jws<Claims> claims = Jwts.parserBuilder().setSigningKey(secretKey).build()
                     .parseClaimsJws(jwtToken);
             return !claims.getBody().getExpiration().before(new Date());
         } catch (Exception e) {
+            LOGGER.info("[validateToken] 토큰 유효 체크 예외 발생");
             return false;
         }
     }
