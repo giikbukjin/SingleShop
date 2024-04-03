e3f2515da2bb9493e41668ef26958b269b1749e1
Jinseo Jeong <js020315@gmail.com>
2024-04-03 16:01:22 +0900

    [FIX] Product
    Product의 전반적인 버그 수정

--- src/main/java/com/elice/team4/singleShop/product/controller/ProductApiController.java
+++ src/main/java/com/elice/team4/singleShop/product/controller/ProductApiController.java
@@ -1,10 +1,12 @@
 package com.elice.team4.singleShop.product.controller;
 
 import com.elice.team4.singleShop.product.domain.Product;
+import com.elice.team4.singleShop.product.dto.ProductDto;
 import com.elice.team4.singleShop.product.mapper.ProductMapper;
 import com.elice.team4.singleShop.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -13,9 +15,8 @@ public class ProductApiController {
     private final ProductService productService;
 
     @PostMapping("/api/seller/new")
-    public String create(ProductForm form) {
-        Product product = ProductMapper.INSTANCE.toProduct(form);
-        productService.saveProduct(product);
+    public String create(@ModelAttribute ProductDto productDto) {
+        productService.saveProduct(productDto); // ProductDto를 직접 전달
         return "redirect:/";
     }
 }
