58e3ad094090858ddd64cf063e6c51b0cc710dca
wjcho <wj930303@gmail.com>
2024-04-04 17:13:56 +0900

    Feat: category validation error pages responding codes.

--- src/main/java/com/elice/team4/singleShop/category/controller/CategoryController.java
+++ src/main/java/com/elice/team4/singleShop/category/controller/CategoryController.java
@@ -7,6 +7,7 @@ import com.elice.team4.singleShop.category.service.CategoryService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -25,7 +26,8 @@ public class CategoryController {
     public String getCategories(Model model) {
         List<Category> categories = categoryService.findCategories();
         model.addAttribute("categories", categories);
-        return "home/home";
+        // TODO: admin/admin 페이지 수정해야 함.
+        return "redirect:/admin/admin.html";
     }
 
     @GetMapping("/{id}")
@@ -33,18 +35,23 @@ public class CategoryController {
         Category foundCategory = categoryService.findCategory(id);
 
         model.addAttribute("category", foundCategory);
-        //TODO: category 를 쏘아 줄 html 페이지 경로 지정
+
         return "category/category";
     }
 
     @GetMapping("/add")
     public String createCategoryGet(Model model) {
-        //TODO: category 생성 html 페이지 경로 지정
+
         return "category-add/category-add";
     }
 
     @PostMapping("/add")
-    public String createCategoryPost(@ModelAttribute @Validated CategoryDto categoryDto) {
+    public String createCategoryPost(@ModelAttribute @Validated CategoryDto categoryDto,
+                                     BindingResult bindingResult) {
+        if(bindingResult.hasErrors()) {
+            return "category-error/category-add-error";
+        }
+
         Category category = categoryMapper.CategoryDtoToCategory(categoryDto);
 
         Category savedCategory = categoryService.createCategory(category);
@@ -58,30 +65,32 @@ public class CategoryController {
         Category category = categoryService.findCategory(id);
         model.addAttribute("category", category);
 
-        //TODO: category 수정 html 페이지 경로 지정
         return "category-edit/category-edit";
     }
 
     @PostMapping("/{id}/edit")
     public String editCategoryPost(@PathVariable Long id,
                                    @ModelAttribute @Validated CategoryDto categoryDto,
-                                   RedirectAttributes redirectAttributes) {
+                                   BindingResult bindingResult) {
+
+        if(bindingResult.hasErrors()) {
+            return "category-error/category-edit-error";
+        }
+
         Category category = categoryMapper.CategoryDtoToCategory(categoryDto);
         Category updatedCategory = categoryService.updateCategory(category, id);
 
-        redirectAttributes.addAttribute("categoryId", updatedCategory.getId());
-        redirectAttributes.addFlashAttribute("message", "카테고리가 수정되었습니다.");
-
-        //TODO: redirect 경로 지정 - 해당 카테고리 상세 페이지
         return "redirect:/admin/category/{id}";
     }
 
     @DeleteMapping("/{id}")
     public String deleteCategory(@PathVariable Long id, RedirectAttributes redirectAttributes) {
         categoryService.deleteCategory(id);
+
+        // Modal 창을 사용할 경우
         redirectAttributes.addFlashAttribute("message", "카테고리가 삭제되었습니다.");
 
         //TODO: redirect 경로 지정 - 카테고리 관리 페이지
-        return "redirect:/admin";
+        return "redirect:/admin/category";
     }
 }
