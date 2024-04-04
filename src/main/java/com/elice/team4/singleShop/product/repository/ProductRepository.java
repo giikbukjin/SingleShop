65808dc783ba81b98475ac63bc6f92c20ce0cd8a
wjcho <wj930303@gmail.com>
2024-04-04 19:31:00 +0900

    Refactor: restore into develop branch codes

--- src/main/java/com/elice/team4/singleShop/product/repository/ProductRepository.java
+++ src/main/java/com/elice/team4/singleShop/product/repository/ProductRepository.java
@@ -5,8 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
-//    List<Product> findAllByProductId(Long id);
+    Optional<Product> findById(Long id);
+
+    // 상품 이름으로 상품 조회
+    List<Product> findByName(String name);
 }
