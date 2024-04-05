2bdeb2c38ef7c23f6e66881530dc1bcea4d9a59a
blaumonde <larazili174@gmail.com>
2024-04-05 13:39:00 +0900

    chore

--- src/main/java/com/elice/team4/singleShop/order/dto/OrderDto.java
+++ src/main/java/com/elice/team4/singleShop/order/dto/OrderDto.java
@@ -8,7 +8,7 @@ import lombok.Setter;
 @Setter
 public class OrderDto {
 
-    //@NotNull(message = "상품 아이디는 필수값입니다.")
+    @NotNull(message = "상품 아이디는 필수값입니다.")
     private Long productId;
 
     private int count;
