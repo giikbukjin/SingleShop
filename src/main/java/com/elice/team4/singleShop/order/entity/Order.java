4f3a6570651dc4a9ca66cbc137c4bd375b6e95c7
blaumonde <larazili174@gmail.com>
2024-04-05 16:23:10 +0900

    chore : create order

--- src/main/java/com/elice/team4/singleShop/order/entity/Order.java
+++ src/main/java/com/elice/team4/singleShop/order/entity/Order.java
@@ -46,6 +46,7 @@ public class Order {
     @LastModifiedDate
     private String modifiedBy; // 수정자
 
+
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user; // 사용자는 여러 개의 주문내역 존재
@@ -62,6 +63,7 @@ public class Order {
     public static Order createOrder(User user, List<OrderItem> orderItemList) {
         Order order = new Order();
         order.setUser(user); // 상품 주문한 회원 정보 세팅
+
         for (OrderItem orderItem : orderItemList) { // 여러 개의 상품 주문 가능
             order.addOrderItem(orderItem);
         }
@@ -83,7 +85,7 @@ public class Order {
     public void cancelOrder() {
         this.orderStatus = OrderStatus.CANCEL;
 
-        for(OrderItem orderItem : orderItems) {
+        for (OrderItem orderItem : orderItems) {
             orderItem.cancel();
         }
     }
