3812dfa925bf2def22e19de44ae1c14a5ba498cb
blaumonde <larazili174@gmail.com>
2024-04-06 04:06:08 +0900

    Feat : 배송 정보 생성

--- src/main/java/com/elice/team4/singleShop/order/service/DeliveryInfoService.java
+++ src/main/java/com/elice/team4/singleShop/order/service/DeliveryInfoService.java
@@ -1,1 +1,27 @@
 package com.elice.team4.singleShop.order.service;
+
+import com.elice.team4.singleShop.order.dto.DeliveryInfoDto;
+import com.elice.team4.singleShop.order.entity.DeliveryInfo;
+import com.elice.team4.singleShop.order.repository.DeliveryInfoRepository;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class DeliveryInfoService {
+
+    private final DeliveryInfoRepository deliveryInfoRepository;
+
+    public DeliveryInfo createDeliveryInfo(DeliveryInfoDto deliveryInfoDto) {
+        DeliveryInfo deliveryInfo = new DeliveryInfo();
+        deliveryInfo.setReceiverName(deliveryInfoDto.getReceiverName());
+        deliveryInfo.setReceiverPhoneNumber(deliveryInfoDto.getReceiverPhoneNumber());
+        deliveryInfo.setPostalCode(deliveryInfoDto.getPostalCode());
+        deliveryInfo.setAddress1(deliveryInfoDto.getAddress1());
+        deliveryInfo.setAddress2(deliveryInfoDto.getAddress2());
+        deliveryInfo.setDeliveryRequest(deliveryInfoDto.getDeliveryRequest());
+
+        return deliveryInfoRepository.save(deliveryInfo);
+    }
+}
