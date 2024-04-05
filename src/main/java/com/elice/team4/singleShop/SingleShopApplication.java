ec8ccb61c1abdd963845b732f10a5abad0185a18
wjcho <wj930303@gmail.com>
2024-04-05 23:11:43 +0900

    Feat: DataInit.

--- src/main/java/com/elice/team4/singleShop/SingleShopApplication.java
+++ src/main/java/com/elice/team4/singleShop/SingleShopApplication.java
@@ -1,7 +1,10 @@
 package com.elice.team4.singleShop;
 
+import com.elice.team4.singleShop.user.repository.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
@@ -10,6 +13,12 @@ public class SingleShopApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SingleShopApplication.class, args);
+	}
+
+	@Bean
+	@Profile(value = {"local", "test", "default"})
+	public DataInit stubDataInit(UserRepository userRepository) {
+		return new DataInit(userRepository);
 	}
 
 }
