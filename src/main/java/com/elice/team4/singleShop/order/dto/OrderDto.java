2a76e3304f0c7966935b6ad5b563afc139464108
blaumonde <larazili174@gmail.com>
2024-04-05 16:22:16 +0900

    fix

--- src/main/java/com/elice/team4/singleShop/order/dto/OrderDto.java
+++ src/main/java/com/elice/team4/singleShop/order/dto/OrderDto.java
@@ -1,5 +1,7 @@
 package com.elice.team4.singleShop.order.dto;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
@@ -11,5 +13,6 @@ public class OrderDto {
     @NotNull(message = "상품 아이디는 필수값입니다.")
     private Long productId;
 
+    @Min(value = 1, message = "최소 주문 수량은 1개 입니다.")
     private int count;
 }
