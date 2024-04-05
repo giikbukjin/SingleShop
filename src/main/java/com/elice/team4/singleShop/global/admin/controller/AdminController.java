af2f77001bba58efc89725d621e95cdc3b51b22e
wjcho <wj930303@gmail.com>
2024-04-05 13:10:30 +0900

    Feat: category detail page initial commit

--- src/main/java/com/elice/team4/singleShop/global/admin/controller/AdminController.java
+++ src/main/java/com/elice/team4/singleShop/global/admin/controller/AdminController.java
@@ -3,12 +3,14 @@ package com.elice.team4.singleShop.global.admin.controller;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @AllArgsConstructor
+@RequestMapping("/admin")
 public class AdminController {
 
-    @GetMapping("/admin")
+    @GetMapping
     public String adminPage() {
         return "admin/admin";
     }
