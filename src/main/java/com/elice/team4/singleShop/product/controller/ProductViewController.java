80c635fa53362503118128b79b184c4915e88bf5
Jinseo Jeong <js020315@gmail.com>
2024-04-06 19:30:23 +0900

    [FEAT] Select Category

--- src/main/java/com/elice/team4/singleShop/product/controller/ProductViewController.java
+++ src/main/java/com/elice/team4/singleShop/product/controller/ProductViewController.java
@@ -1,5 +1,7 @@
 package com.elice.team4.singleShop.product.controller;
 
+import com.elice.team4.singleShop.category.entity.Category;
+import com.elice.team4.singleShop.category.service.CategoryService;
 import com.elice.team4.singleShop.product.domain.Product;
 import com.elice.team4.singleShop.product.dto.ProductDto;
 import com.elice.team4.singleShop.product.mapper.ProductMapper;
@@ -18,9 +20,12 @@ import java.util.Optional;
 public class ProductViewController {
     private final ProductService productService;
     private final ProductMapper productMapper;
+    private final CategoryService categoryService;
 
     @GetMapping("/seller/new")
     public String createForm(Model model) {
+        List<Category> categories = categoryService.findCategories(); // 모든 카테고리를 가져오는 메서드 호출
+        model.addAttribute("categories", categories);
         model.addAttribute("productDto", new ProductDto());
         return "products/add/product-add";
     }
@@ -35,11 +40,14 @@ public class ProductViewController {
     @GetMapping("/seller/{productId}")
     public String updateProductForm(@PathVariable("productId") Long productId, Model model) {
         Optional<Product> productOptional = productService.findProductById(productId);
-
         if (productOptional.isPresent()) {
             Product product = productOptional.get();
             ProductDto productDto = productMapper.productToProductDto(product);
+
+            List<Category> categories = categoryService.findCategories(); // 모든 카테고리를 가져오는 메서드 호출
+            model.addAttribute("categories", categories);
             model.addAttribute("productDto", productDto);
+
             return "products/edit/product-edit";
         } else {
             return "redirect:/products";
