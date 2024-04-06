cc287bc929e32672e36c6a831a683e22bec2c609
Jinseo Jeong <js020315@gmail.com>
2024-04-07 01:35:35 +0900

    [FEAT] Product Category

--- src/main/java/com/elice/team4/singleShop/product/controller/ProductViewController.java
+++ src/main/java/com/elice/team4/singleShop/product/controller/ProductViewController.java
@@ -44,8 +44,8 @@ public class ProductViewController {
             Product product = productOptional.get();
             ProductDto productDto = productMapper.productToProductDto(product);
 
-            // 기존 상품의 카테고리 정보를 유지하기 위해 categoryId 설정
-            productDto.setId(product.getCategory().getId());
+            // 카테고리 ID 설정
+            productDto.setCategoryId(product.getCategory().getId());
 
             List<Category> categories = categoryService.findCategories();
             model.addAttribute("categories", categories);
--- src/main/java/com/elice/team4/singleShop/product/mapper/ProductMapper.java
+++ src/main/java/com/elice/team4/singleShop/product/mapper/ProductMapper.java
@@ -9,13 +9,14 @@ import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring", uses = {CategoryMapper.class})
 public interface ProductMapper {
-    @Mapping(source = "category", target = "category")
+
+    @Mapping(source = "category.id", target = "categoryId")
     ProductDto productToProductDto(Product product);
 
-    @Mapping(source = "category", target = "category")
+    @Mapping(target = "category", ignore = true) // DTO에서 엔티티로 변환 시 카테고리는 수동으로 처리
     Product productDtoToProduct(ProductDto productDto);
-
 
     @Mapping(target = "id", ignore = true) // ID 필드는 매핑에서 제외
     void updateProductFromDto(ProductDto dto, @MappingTarget Product entity);
 }
+
