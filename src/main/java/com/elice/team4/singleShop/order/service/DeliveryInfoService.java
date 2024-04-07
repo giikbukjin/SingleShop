822ebad33f33db652b1fe5fcc2bde8e53425c3ec
blaumonde <larazili174@gmail.com>
2024-04-07 22:35:04 +0900

    Feat : delivery

--- src/main/java/com/elice/team4/singleShop/order/service/DeliveryInfoService.java
+++ src/main/java/com/elice/team4/singleShop/order/service/DeliveryInfoService.java
@@ -3,7 +3,6 @@ package com.elice.team4.singleShop.order.service;
 import com.elice.team4.singleShop.order.dto.DeliveryInfoDto;
 import com.elice.team4.singleShop.order.entity.DeliveryInfo;
 import com.elice.team4.singleShop.order.repository.DeliveryInfoRepository;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,8 +12,11 @@ public class DeliveryInfoService {
 
     private final DeliveryInfoRepository deliveryInfoRepository;
 
+    // 새로운 배송 정보 생성 & 저장
     public DeliveryInfo createDeliveryInfo(DeliveryInfoDto deliveryInfoDto) {
-        DeliveryInfo deliveryInfo = new DeliveryInfo();
+        DeliveryInfo deliveryInfo = new DeliveryInfo(); // DeliveryInfo 객체 생성
+
+        // Dto로 받은 정보를 DeliveryInfo 객체에 설정
         deliveryInfo.setReceiverName(deliveryInfoDto.getReceiverName());
         deliveryInfo.setReceiverPhoneNumber(deliveryInfoDto.getReceiverPhoneNumber());
         deliveryInfo.setPostalCode(deliveryInfoDto.getPostalCode());
@@ -22,6 +24,6 @@ public class DeliveryInfoService {
         deliveryInfo.setAddress2(deliveryInfoDto.getAddress2());
         deliveryInfo.setDeliveryRequest(deliveryInfoDto.getDeliveryRequest());
 
-        return deliveryInfoRepository.save(deliveryInfo);
+        return deliveryInfoRepository.save(deliveryInfo); // 생성된 배송 정보 저장
     }
 }
