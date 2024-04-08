e9e8ba204977a1b8988a24642e4303b9b04b5c0e
blaumonde <larazili174@gmail.com>
2024-04-08 16:27:35 +0900

    Chore : update deliveryInfo

--- src/main/java/com/elice/team4/singleShop/order/controller/DeliveryInfoController.java
+++ src/main/java/com/elice/team4/singleShop/order/controller/DeliveryInfoController.java
@@ -3,6 +3,7 @@ package com.elice.team4.singleShop.order.controller;
 import com.elice.team4.singleShop.order.dto.DeliveryInfoDto;
 import com.elice.team4.singleShop.order.entity.DeliveryInfo;
 import com.elice.team4.singleShop.order.service.DeliveryInfoService;
+import com.elice.team4.singleShop.order.service.OrderService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -14,23 +15,10 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class DeliveryInfoController {
 
-    private final DeliveryInfoService deliveryInfoService;
+    private final OrderService orderService;
 
-    @PostMapping
-    public ResponseEntity<?> createDeliveryInfo(@RequestBody @Valid DeliveryInfoDto deliveryInfoDto,
-                                                BindingResult bindingResult) { // 요청 본문을 Dto 객체로 매핑, 전달된 Dto 객체 유효성 검사
-        // 유효성 검사 결과 오류 있는 경우
-        if (bindingResult.hasErrors()) {
-            return ResponseEntity.badRequest().body(bindingResult.getAllErrors());
-        }
-
-        // 배송 정보 생성 & 저장
-        DeliveryInfo savedDeliveryInfo = deliveryInfoService.createDeliveryInfo(deliveryInfoDto);
-        return ResponseEntity.ok(savedDeliveryInfo);
-    }
-
-    @PutMapping("/{deliveryInfoId}")
-    public ResponseEntity<?> updateDeliveryInfo(@PathVariable Long deliveryInfoId,
+    @PutMapping("/{orderId}/delivery")
+    public ResponseEntity<?> updateDeliveryInfo(@PathVariable Long orderId,
                                                 @RequestBody @Valid DeliveryInfoDto deliveryInfoDto,
                                                 BindingResult bindingResult) {
         // 데이터 바인딩 시 에러 유무 검사
@@ -39,7 +27,7 @@ public class DeliveryInfoController {
         }
 
         // 배송 정보 수정
-        DeliveryInfo updateDeliveryInfo = deliveryInfoService.updateDeliveryInfo(deliveryInfoId, deliveryInfoDto);
+        DeliveryInfo updateDeliveryInfo = orderService.updateDeliveryInfo(orderId, deliveryInfoDto);
         return ResponseEntity.ok(updateDeliveryInfo);
     }
 }
