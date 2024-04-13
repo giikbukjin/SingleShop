74831537c5ce0d77778fa33a649bbecf909a4813
blaumonde <larazili174@gmail.com>
2024-04-13 22:08:11 +0900

    Chore

--- src/main/java/com/elice/team4/singleShop/order/controller/AdminController.java
+++ src/main/java/com/elice/team4/singleShop/order/controller/AdminController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
-@RequestMapping("/admin/orders")
+//@RequestMapping("/admin/orders")
 @RequiredArgsConstructor
 public class AdminController {
 
