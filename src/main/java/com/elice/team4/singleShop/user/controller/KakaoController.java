d8891adf2d5e4ff32413565afe2c8d6743c3f622
Wambatcode <feyfey927@gmail.com>
2024-04-14 14:52:35 +0900

    Feat: 카카오 로그아웃(Access Token만료 시키기)기능 구현

--- src/main/java/com/elice/team4/singleShop/user/controller/KakaoController.java
+++ src/main/java/com/elice/team4/singleShop/user/controller/KakaoController.java
@@ -1,16 +1,24 @@
 package com.elice.team4.singleShop.user.controller;
 
 import com.elice.team4.singleShop.user.oauth.KakaoApi;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.Arrays;
 import java.util.Map;
+import java.util.Optional;
 
 @Controller
 @RequestMapping
+@Slf4j
 public class KakaoController {
     private final KakaoApi kakaoApi;
 
@@ -31,10 +39,32 @@ public class KakaoController {
     }
 
     @RequestMapping("${kakao.logoutRedirect_shortUri}")
-    public String kakaoLogout(@RequestParam String code, HttpServletResponse response){
-        String accessToken = kakaoApi.getAccessToken(code, response);
-        System.out.println(accessToken);
+    public String kakaoLogout(HttpServletRequest request, HttpServletResponse response) throws UnsupportedEncodingException {
+        String accessToken = "";
+        if (request.getCookies() != null) {
+            Optional<Cookie> tokenCookie = Arrays.stream(request.getCookies())
+                    .filter(
+                            cookie -> cookie.getName().equals("Authorization")
+                    ).findFirst();
+
+            if (tokenCookie.isPresent()) {
+                String token = URLDecoder.decode(tokenCookie.get().getValue(), "UTF-8");
+                if (token != null && token.startsWith("Bearer ")) {
+                    accessToken = token.substring(7);
+                }
+            }
+        }
+
         kakaoApi.kakaoLogout(accessToken);
+
+        for (Cookie cookie : request.getCookies()) {
+            String cookieName = cookie.getName();
+            Cookie cookieToDelete = new Cookie(cookieName, null);
+            cookieToDelete.setMaxAge(0);
+            cookieToDelete.setPath("/");
+            response.addCookie(cookieToDelete);
+        }
+        log.info("[kakaoLogout] 카카로 로그아웃 완료");
         return "redirect:/home";
     }
 }
