57506a5fa74cbf680057a29604c3a4b2d330619c
mkj <menatical@gmail.com>
2024-04-15 15:59:28 +0900

    product modify

--- src/main/java/com/elice/team4/singleShop/product/controller/ProductViewController.java
+++ src/main/java/com/elice/team4/singleShop/product/controller/ProductViewController.java
@@ -6,12 +6,15 @@ import com.elice.team4.singleShop.product.domain.Product;
 import com.elice.team4.singleShop.product.dto.ProductDto;
 import com.elice.team4.singleShop.product.mapper.ProductMapper;
 import com.elice.team4.singleShop.product.service.ProductService;
+import com.elice.team4.singleShop.user.entity.User;
+import com.elice.team4.singleShop.user.service.UserDetailsServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.security.Principal;
 import java.util.List;
 import java.util.Optional;
 
@@ -21,6 +24,7 @@ public class ProductViewController {
     private final ProductService productService;
     private final ProductMapper productMapper;
     private final CategoryService categoryService;
+    private final UserDetailsServiceImpl userService;
 
     @GetMapping("/seller/new")
     public String createForm(Model model) {
@@ -61,11 +65,27 @@ public class ProductViewController {
     }
 
     @GetMapping("/products/{productId}")
-    public String showProductDetail(@PathVariable Long productId, Model model) {
-        Product product = productService.findProductById(productId)
-                .orElseThrow(() -> new RuntimeException("Product not found!"));
-        model.addAttribute("product", product);
-        return "products/detail/product-detail"; // 상품 상세 페이지의 HTML 파일 이름
+    public String getProductDetail(@PathVariable Long productId, Model model, Principal principal) {
+
+        Optional<Product> productOptional = productService.findProductById(productId);
+
+        if (productOptional.isPresent()) {
+
+            Product product = productOptional.get();
+            model.addAttribute("product", product);
+
+
+            String currentUserName = principal.getName();
+
+            User currentUser = (User) userService.loadUserByUsername(currentUserName);
+            model.addAttribute("currentUser", currentUser);
+
+            return "products/detail/product-detail"; // product-detail은 상품 상세 정보 페이지의 이름입니다. 실제로는 해당하는 템플릿이 존재해야 합니다.
+        } else {
+
+            model.addAttribute("errorMessage", "상품을 찾을 수 없습니다.");
+            return "error-page"; // error-page는 오류 페이지의 이름입니다. 실제로는 해당하는 템플릿이 존재해야 합니다.
+        }
     }
 
     @GetMapping("/seller/products/{productId}")
