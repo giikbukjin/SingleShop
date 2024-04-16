b32453d61f4303ea4b739f80421791e7cb000b94
[Cloud2]최윤기 <cwanthigh@gmail.com>
2024-04-16 17:21:58 +0900

    fix:user Entity name, navbar

--- src/main/resources/static/js/navbar.js
+++ src/main/resources/static/js/navbar.js
@@ -99,11 +99,32 @@ const addNavElements = (keyString) => {
     login: '<li><a href="/auth/login">로그인</a></li>',
   };
 
-// 여기랑 밑에 주석 풀어주셔야 카카오 로그아웃 사용 가능합니다.
+// 여기랑 밑에 주석 풀어주셔야 카카오 로그아웃 사용 가능합니다. 다양한 방법 존재. HTTPONLY 속성의 쿠키를 불러올 수 있는 방법?
 //  let logoutPath;
-//  if (getCookie("kakao")) { logoutPath = '<li><a href="https://kauth.kakao.com/oauth/logout?client_id=5ac62ccca7ee659af683fca629357a50&logout_redirect_uri=http://localhost:8080/auth/logout" id="logout">로그아웃</a></li>' }
+//
+//      function cookieCheck(name) {
+//            const cookies = document.cookie.split(';');
+//                for (const cookie of cookies) {
+//                    const [cookieName, cookieValue] = cookie.trim().split('=');
+//                        if (cookieName === name) {
+//                            return true;
+//                        }
+//                }
+//            return null;
+//          }
+//
+//          function getCookieName(name) {
+//            let matches = document.cookie.match(new RegExp(
+//              "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+//            ));
+//            return matches ? decodeURIComponent(matches[1]) : undefined;
+//          }
+////
+//  console.log(getCookieName("kakao"));
+//
+//  if (cookieCheck("kakao")) { logoutPath = '<li><a href="https://kauth.kakao.com/oauth/logout?client_id=1fa06f949c45fbf2a3e893c494263777&logout_redirect_uri=http://localhost:8080/auth/logout" id="logout">로그아웃</a></li>' }
 //      else { logoutPath = '<li><a href="/auth/logout" id="logout">로그아웃</a></li>' };
-
+//
 //      console.log(logoutPath);
 
   // 로그인 완료된 상태에서만 보이게 될 navbar 요소들
@@ -127,34 +148,35 @@ const addNavElements = (keyString) => {
       if (logoutElem) {
         logoutElem.addEventListener('click', () => {
           window.location.href = '/auth/logout';
-          window.location.reload();
+//          window.location.reload(); 혹시나, 사이트에 그대로 남아있는 경우를 위해서
         });
       }
   `;
 
-// 비동기 함수로 하니까 안 되서 아래 방법 사용했는데, 함수 안에 내용이 바뀌지 않습니다. 왜 그럴까요?
 
-//  var isAdmin = false;
-//
-//    var xhr = new XMLHttpRequest();
-//    xhr.open('GET', '/api/users/admin-check', true); // GET 요청을 보냄
-//    xhr.onreadystatechange = function () {
-//      if (xhr.readyState === XMLHttpRequest.DONE) { // 요청이 완료되면
-//        if (xhr.status === 200) { // 요청이 성공했을 때
-//          console.log(xhr.responseText); // 응답 데이터를 출력
-//          console.log("\"status\": \"success\"");
-//          if (xhr.responseText == "{\"status\": \"success\"}") {
-//              isAdmin = true;
-//              console.log("checkAdmin, if 구문 안에서 :", isAdmin);
-//          }
-//
-//        } else {
-//          console.error('Request failed with status:', xhr.status); // 요청이 실패했을 때
-//        }
-//      }
-//    };
-//    xhr.send(); // 요청 보내기
-//    console.log("요청 보내고 난 후,", isAdmin);
+// 비동기 함수로 하니까 isAdmin값을 넣을 수가 없습니다. 안 되서 아래 방법 사용했는데, 함수 안에서는 true로 바뀌나,
+// isAdmin이 밖에서는 내용이 바뀌지 않습니다. 왜 그럴까요?
+
+  var isAdmin = false;
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', '/api/users/admin-check', true); // GET 요청을 보냄
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === XMLHttpRequest.DONE) { // 요청이 완료되면
+        if (xhr.status === 200) { // 요청이 성공했을 때
+          if (xhr.responseText == "{\"status\": \"success\"}") {
+              isAdmin = true;
+              console.log("if 안쪽의 isAdmin 값: ", isAdmin);
+          }
+
+        } else {
+          console.error('Request failed with status:', xhr.status); // 요청이 실패했을 때
+        }
+      }
+    };
+    xhr.send(); // 요청 보내기
+
+    console.log("요청 보내고 난 후의 is Admin: ", isAdmin);
 
   let items = "";
   for (const key of keys) {
